67cfe34a7161ebf62af1c3bfb7966964d4878b48
Maciej Wójcik <wojcik91@gmail.com>
2018-12-19 12:34:25 +0100

    Sort data into training dataset and values to predict

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import pandas as pd
 
+def sort_input_data(df):
+    data = []
+    pred = []
+    for index, row in df.iterrows():
+        if pd.isnull(row[1]):
+            pred.append(row)
+        else:
+            data.append(row)
+
+    return (data, pred)
+        
+
 if __name__ == "__main__":
     salary_df = pd.read_csv('salary.csv', na_values=['', ' '])
     print(salary_df)
+    (salary_data, salary_pred) = sort_input_data(salary_df)
